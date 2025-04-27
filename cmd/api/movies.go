815e8bc78539439f9f0c6e7a739354f182a1a721
shrigharat <gharatshri014@gmail.com>
2025-04-27 12:43:26 +0530

    added reusable error handling functions

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -15,22 +15,21 @@ func (app *application) createMovieHandler(w http.ResponseWriter, r *http.Reques
 func (app *application) showMovieHandler(w http.ResponseWriter, r *http.Request) {
 	id, err := app.readIDParam(w, r)
 	if err != nil {
-		http.NotFound(w, r)
+		app.notFoundResponse(w, r)
 		return
 	}
-	
+
 	movie := data.Movie{
-		ID: id,
+		ID:        id,
 		CreatedAt: time.Now(),
-		Title: "Casablanca",
-		Runtime: 102,
-		Genres: []string{"drama", "romance", "war"},
-		Version: 1,
+		Title:     "Casablanca",
+		Runtime:   102,
+		Genres:    []string{"drama", "romance", "war"},
+		Version:   1,
 	}
 
 	err = app.writeJSON(w, http.StatusOK, envelope{"movie": movie}, nil)
-	if err!=nil{
-		app.logger.Error(err.Error())
-		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
 	}
 }
