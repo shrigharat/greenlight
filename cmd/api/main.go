95d97ee473ba7236f5d0c394c308cd5e9646c4ad
shrigharat <gharatshri014@gmail.com>
2025-05-01 15:03:28 +0530

    added dotenv package to load .env file, added basic DB connection logic

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log/slog"
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 const version = "1.0.0"
@@ -14,6 +19,9 @@ const version = "1.0.0"
 type config struct {
 	port int
 	env  string
+	db struct {
+		dsn string
+	}
 }
 
 type application struct {
@@ -24,13 +32,30 @@ type application struct {
 func main() {
 	var cfg config
 
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		os.Exit(1)
+	}
+
 	flag.IntVar(&cfg.port, "port", 4000, "Port to run the server on")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "Postgres DSN")
 	flag.Parse()
 
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
 		AddSource: true,
 	}))
+
+	db, err := openDB(cfg)
+	if err!=nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("database connection pool established")
+	defer db.Close()
 
 	app := application{
 		config: cfg,
@@ -52,8 +77,26 @@ func main() {
 
 	logger.Info("starting server", "addr", server.Addr, "env", cfg.env)
 
-	err := server.ListenAndServe()
+	err = server.ListenAndServe()
 
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(cfg config) (*sql.DB, error) {
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
