e6255b1c28bd98ce9af8b31787565e3d3668b779
shrigharat <gharatshri014@gmail.com>
2025-05-11 17:58:14 +0530

    added token logic, DB handling and template changes

--- cmd/api/users.go
+++ cmd/api/users.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"net/http"
+	"time"
 
 	"greenlight.shrishail.dev/internal/data"
 	"greenlight.shrishail.dev/internal/validator"
@@ -54,8 +55,24 @@ func (app *application) registerUserHandler(w http.ResponseWriter, r *http.Reque
 		return
 	}
 
+	token, err := app.models.Tokens.New(user.ID, 3*24*time.Hour, data.ActivationScope)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
+	err = app.models.Tokens.Insert(token)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+		return
+	}
+
 	app.background(func() {
-		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", user)
+		data := map[string]any{
+			"activationToken": token.PlainText,
+			"userID":          user.ID,
+		}
+		err = app.mailer.Send(user.Email, "user_welcome.tmpl.html", data)
 		if err != nil {
 			app.logger.Error(err.Error())
 		}
