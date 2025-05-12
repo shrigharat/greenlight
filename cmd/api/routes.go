6b714e6e41da85f2c1fc827ef3fbbc043c234965
shrigharat <gharatshri014@gmail.com>
2025-05-12 11:36:05 +0530

    added authentication token creation logic

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -26,5 +26,8 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
+	// tokens routes
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
 	return app.recoverPanic(app.rateLimit(router))
 }
